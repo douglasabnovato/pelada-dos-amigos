ee1f4b7f136d499288fbaa31807ca85490fcfe4e
Douglas A B Novato <douglasabnovato@gmail.com>
2022-07-26 10:06:34 -0300

    alteração na estrutura de pastas para fazer deploy no github pages

--- main.jsx
+++ main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./pages/Home";
-import "./styles/global.css";
+import Home from "./src/pages/Home";
+import "./src/styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Home />
